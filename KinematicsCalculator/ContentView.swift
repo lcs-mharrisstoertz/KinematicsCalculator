51c9fa0bccb1bfcd7a34b9714d83b7776ecfe113
Morgan <Mharrisstoertz@gmail.com>
2023-06-05 11:50:20 -0400

    got LaTex working

--- KinematicsCalculator/ContentView.swift
+++ KinematicsCalculator/ContentView.swift
@@ -4,16 +4,17 @@
 //
 //  Created by Morgan Harris-Stoertz on 2023-06-05.
 //
-
+import LaTeXSwiftUI
 import SwiftUI
 
+
 struct ContentView: View {
+    @State var a = 0.0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            LaTeX("Pythagorean Theorem: $$\(a)^2 + b^2 = c^2$$")
+            
+            Slider(value: $a, in: 0...100, step: 1.0)
         }
         .padding()
     }
