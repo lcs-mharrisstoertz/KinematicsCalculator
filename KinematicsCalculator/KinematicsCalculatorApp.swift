561d91171d4d2fe6ae438506f3e697843f0e46d0
Morgan <Mharrisstoertz@gmail.com>
2023-06-05 11:38:37 -0400

    Initial Commit

---
+++ KinematicsCalculator/KinematicsCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinematicsCalculatorApp.swift
+//  KinematicsCalculator
+//
+//  Created by Morgan Harris-Stoertz on 2023-06-05.
+//
+
+import SwiftUI
+
+@main
+struct KinematicsCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
